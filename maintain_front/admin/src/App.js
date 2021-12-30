bbb630b8bff2135b519ee217845825908310566c
Dhairya <56044321+Dhairya2811@users.noreply.github.com>
2021-12-29 22:10:43 -0500

    Update App.js

--- maintain_front/admin/src/App.js
+++ maintain_front/admin/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
 import Admin_start from './components/Admin_start/Admin_start';
 import './App.css';
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://das-quiz.herokuapp.com/";
 
 function App() {
   const socket  = socketIOClient(ENDPOINT);
